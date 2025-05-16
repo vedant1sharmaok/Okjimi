00971d77cf79707b1e830bd55b6bea5abb3fa928
vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>
2025-05-16 18:34:27 +0530

    Update config.py
    Signed-off-by: vedant1sharmaok <135007288+vedant1sharmaok@users.noreply.github.com>

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", 23053980))
+API_HASH = getenv("API_HASH", 9f7cb9215fa0007089951c86d31d0128)
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
